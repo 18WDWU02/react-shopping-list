634527abdf4a725d4cf6a2766098ab1e867e16a3
RichardHpaYoobee <richard.hpa@acgedu.com>
2018-08-31 09:17:02 +1200

    added states to the list

--- src/index.js
+++ src/index.js
@@ -5,24 +5,62 @@ import './index.css';
 import Form from './form';
 
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            list: [
+                {
+                    id: 1,
+                    item: 'Apples'
+                },
+                {
+                    id: 2,
+                    item: 'Pears'
+                },
+                {
+                    id: 3,
+                    item: 'Bananas'
+                }
+
+            ],
+            text: 'Hello World'
+        }
+
+        this.changeText = this.changeText.bind(this);
+    }
+
+
     render(){
         return (
             <div className="App">
               <div className="jumbotron text-center">
                 <h1 className="display-4">Shopping List</h1>
-
+                <h3>{this.state.text}</h3>
                 {/* Calling the ShoppingList component at the bottom of the page */}
-                <ShoppingList/>
+                <ShoppingList list={this.state.list}/>
 
                 <hr/>
 
                 {/* Calling the Form component which is in ./form.js */}
                 <Form/>
-
+                <button onClick={this.changeText} >Change State of H3</button>
               </div>
             </div>
         )
     }
+
+    changeText(e){
+        e.preventDefault();
+        // alert('H3 will be changed');
+        this.setState({text: 'Button has been clicked'});
+    }
+
+
+
+
+
+
 }
 
 class ShoppingList extends Component{
@@ -30,11 +68,15 @@ class ShoppingList extends Component{
         return(
             <div>
                 <ul className="list-group">
-                    <li className="list-group-item">Cras justo odio</li>
-                    <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Morbi leo risus</li>
-                    <li className="list-group-item">Porta ac consectetur ac</li>
-                    <li className="list-group-item">Vestibulum at eros</li>
+                    {
+                        this.props.list.map(product => {
+                            return <li key={product.id} product={product} className="list-group-item">{product.item}</li>
+                        })
+                    }
+
+
+
+
                 </ul>
             </div>
         )
